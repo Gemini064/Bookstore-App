5bfd2e9c7817294a815a8c67847dd20afd55c538
skylar <yutienh@student.unimelb.edu.au>
2023-02-04 17:19:52 +1100

    build: initialise the database of the bookstore app

---
+++ MvcBookstore/MvcBookstore/Models/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcBookstore.Data;
+using System;
+using System.Linq;
+
+namespace MvcBookstore.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcBookstoreContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcBookstoreContext>>()))
+        {
+            // Look for any books.
+            if (context.Book.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Book.AddRange(
+                new Book
+                {
+                    Title = "CQRS for Dummies"
+                },
+                new Book
+                {
+                    Title = "Visual Studio Tips"
+                },
+                new Book
+                {
+                    Title = "NHibernate Cookbook"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
